424e89e811c9db240f0679aa3f4d041f0b562564
Ryan Long <ryan.long@noaa.gov>
2021-12-27 17:21:43 -0500

    Add missing abstract

--- python_scripts/schedulers/noscheduler.py
+++ python_scripts/schedulers/noscheduler.py
@@ -5,5 +5,9 @@ class NoScheduler(Scheduler):
     TEMPLATE_PATH = None
     SCHEDULER_TYPE = "None"
 
+    @property
+    def template_data(self):
+        return {}
+
     def checkqueue(self, _):
         return True
