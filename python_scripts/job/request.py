3ff0f9e0cf6080c554577a588c3ff2768f84f265
Ryan Long <ryanlong1004@gmail.com>
2022-03-07 13:44:23 -0500

    document and clean up request module

--- python_scripts/job/request.py
+++ python_scripts/job/request.py
@@ -1,18 +1,24 @@
+"""
+Job Abstraction
+"""
+
 import abc
-from typing import Iterable
+import reprlib
+from typing import Any, Dict, Generator, Iterable
 import yaml
 
 
 class Base(abc.ABC):
-    def __init__(self, type, **entries):
-        self.type = type
+    """Job Base Class"""
+    def __init__(self, _type: str, **entries: Dict[str, Any]):
+        self.type = _type
         self.__dict__.update(entries)
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}/>"
+        return f"<{self.__class__.__name__} {reprlib.repr(self.__dict__)}/>"
 
     def __getattr__(self, item):
-        return self[item]
+        return self.__dict__[item]
 
     def __str__(self):
         return str(self.__dict__)
@@ -22,23 +28,28 @@ class Base(abc.ABC):
 
 
 class JobRequest(Base):
+    """represent a job request"""
     @property
     def compilers(self):
+        """yields compilers"""
         for item in self.__dict__["compiler"]:
             yield Compiler(item, **self.__getattribute__(item))
 
 
 class Compiler(Base):
+    """holds compiler information"""
     @property
-    def versions(self):
+    def versions(self)-> Generator["Version", None, None]:
+        """yields versions"""
         for item in self.__dict__["versions"]:
             yield Version(item, **self.__dict__["versions"][item])
 
 
 class Version(Base):
-    """the lowest leaf in the yaml file"""
+    """holds version information"""
 
 
 def read_yaml():
-    with open("./tests/fixtures/cheyenne.yaml") as _file:
+    """convenience for getting a JobRequest from yaml file"""
+    with open("./tests/fixtures/cheyenne.yaml", "r") as _file:
         return JobRequest("JOB", **yaml.safe_load(_file))
